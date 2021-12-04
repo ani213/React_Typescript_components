ba327f51da5de106cb681b3d473a1c1cc9c0e5ed
ani213 <vermaaniket213@gmail.com>
2021-12-04 17:44:10 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import * as React from 'react';
+const App: React.FC<any> = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+       <h1>Hello World</h1>
+    </>
   );
-}
+};
 
 export default App;
